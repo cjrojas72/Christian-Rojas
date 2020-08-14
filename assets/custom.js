24b3bcca5455b9146eb8f4c7b6b8257e99cce5e1
Christian Rojas <cjrojas72@gmail.com>
2020-08-14 01:44:49 -0400

    update to page

--- assets/custom.js
+++ assets/custom.js
@@ -1,48 +1,13 @@
-let toggle = false;
+buttonPos = document.getElementById("main-btns");
 
-let nav = document.querySelector(".nav");
-
-const $about = document.querySelector(".about-sec");
-const $projects = document.querySelector(".portfolio-div");
-const $resume = document.querySelector(".resume-sec");
-const $contact = document.querySelector(".contact-sec");
-const $home = document.querySelector(".centerH");
-
-let navFunc = function (nav) {
-  if (nav === "centerH") {
-    $home.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-  }
-  if (nav === "About") {
-    $about.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-  }
-  if (nav === "Projects") {
-    console.log("you clicked Projects");
-    $projects.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-  }
-  if (nav === "Resume") {
-    $resume.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-  }
-  if (nav === "Contact") {
-    $contact.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-  }
+window.onscroll = function () {
+  scrollFunction();
 };
+
+function scrollFunction() {
+  if (buttonPos.offsetTop < window.scrollY) {
+    document.querySelector("nav").style.visibility = "visible";
+  } else {
+    document.querySelector("nav").style.visibility = "hidden";
+  }
+}
